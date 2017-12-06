5bc41021c5fd9049df1b9f577dfd8cd405655bd4
张冰 <zhangbing1@xin.com>
2017-12-06 11:09:52 +0800

    two-sum

--- src/main/java/com/jaravan/leetcode/TwoSum.java
+++ src/main/java/com/jaravan/leetcode/TwoSum.java
@@ -1,5 +1,8 @@
 package com.jaravan.leetcode;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by icer on 2017/12/6.
  */
@@ -11,12 +14,29 @@ public class TwoSum {
      * Given nums = [2, 7, 11, 15], target = 9,
      * Because nums[0] + nums[1] = 2 + 7 = 9,
      * return [0, 1].
+     *
+     * 典型的"2sum"问题
+     * 时间复杂度  O(n)
      */
 
-    public int[] twoSum(int[] nums, int target) {
+    public static int[] twoSum(int[] nums, int target) {
+        int[] result = new int[2];
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(target - nums[i])) {
+                result[1] = i + 1;
+                result[0] = map.get(target - nums[i]);
+                return result;
+            }
+            map.put(nums[i], i + 1);
+        }
+        return result;
+    }
 
-        return null;
-
+    public static void main(String[] args) {
+        int[] array = new int[]{2, 3, 4, 7, 8};
+        int[] result = twoSum(array, 11);
+        System.out.println(result[0] + "   " + result[1]);
     }
 
 }
